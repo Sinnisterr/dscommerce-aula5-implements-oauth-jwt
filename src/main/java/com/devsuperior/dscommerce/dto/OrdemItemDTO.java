c2126ac13ba557b68e10c99bb08b96489e1d28d0
Willian Bruno <mestrebruno@gmail.com>
2025-06-30 05:37:20 -0300

    Salvando novo pedido

--- src/main/java/com/devsuperior/dscommerce/dto/OrdemItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrdemItemDTO.java
@@ -4,7 +4,7 @@ import com.devsuperior.dscommerce.entities.OrderItem;
 
 public class OrdemItemDTO {
 
-    private Long ProductId;
+    private Long productId;
     private String name;
     private Double price;
     private Integer quantity;
@@ -13,32 +13,48 @@ public class OrdemItemDTO {
     }
 
     public OrdemItemDTO(Long productId, String name, Double price, Integer quantity) {
-        ProductId = productId;
+        this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
 
     }public OrdemItemDTO(OrderItem entity) {
-       this.ProductId = entity.getProduct().getId();
+       this.productId = entity.getProduct().getId();
         this.name = entity.getProduct().getName();
         this.price = entity.getPrice();
         this.quantity = entity.getQuantity();
     }
 
     public Long getProductId() {
-        return ProductId;
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Double getPrice() {
         return price;
     }
 
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
 
     public Double getSubtotal() {
